375876ecaa1d3b0e267a819507f161f5636a309a
Renat Khaybullin <zabzari@gmail.com>
2020-09-04 19:28:18 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int amount = 500000000;
+        int total = balance + amount;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
