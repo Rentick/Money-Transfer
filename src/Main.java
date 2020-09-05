507205d6e59599f206f480f5b980695211deab73
Renat Khaybullin <zabzari@gmail.com>
2020-09-05 18:47:15 +0300

    Modified main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         int balance = 2000000000;
         int amount = 500000000;
         int total = balance + amount;
-        double totalBonus = regularBonus + specialBonus;
-        System.out.println(totalBonus);
+        System.out.println(total);
     }
 }
